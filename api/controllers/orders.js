e6b9906bf1ba385e64b707db7845995e4ee267bf
dobri000 <dimitrije.dobrijevic@gmail.com>
2024-05-02 13:22:25 +0200

    Added controllers

---
+++ api/controllers/orders.js
@@ -0,0 +1,108 @@
+const Order = require("../models/order");
+
+const getAll = (req, res, next) => {
+    Order.find()
+        .select("_id product quantity")
+        .populate("product", "_id name price") // da bi nam vratio zapravo taj product a ne samo njegov id, drugi parametar sta zelimo da nam se priakaze
+        .exec()
+        .then((docs) => {
+            const response = {
+                // pravimo da response objekat ima broj elemenata koje vraca kao i proizvode sa posebnim atributima, kao npr url za svaki
+                count: docs.length,
+                orders: docs.map((doc) => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3000/orders/" + doc._id,
+                        },
+                    };
+                }),
+            };
+            res.status(200).json(response);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+            });
+        });
+}
+
+const createOrder = (req, res, next) => {
+    Product.findById(req.body.productId)
+        .then((product) => {
+            if (!product) {
+                return res.status(404).json({
+                    message: "Product not found",
+                });
+            }
+            const order = new Order({
+                _id: new mongoose.Types.ObjectId(),
+                quantity: req.body.quantity,
+                product: req.body.productId,
+            });
+            return order.save();
+        })
+        .then((result) => {
+            console.log(result);
+            res.status(201).json({
+                message: "Order stored",
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity,
+                },
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: err,
+            });
+        });
+}
+
+const getById = (req, res, next) => {
+    Order.findById(req.params.orderId)
+        .populate("product", "_id name price")
+        .exec()
+        .then((order) => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found",
+                });
+            }
+            res.status(200).json({
+                order: order,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+            });
+        });
+}
+
+const deleteOrder = (req, res, next) => {
+    Order.deleteOne({ _id: req.params.orderId })
+        .exec()
+        .then((result) => {
+            res.status(200).json({
+                message: "Order deleted",
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+            });
+        });
+}
+
+module.exports = {
+    getAll,
+    createOrder,
+    getById,
+    deleteOrder
+}
---
+++ api/controllers/user.js
@@ -0,0 +1,109 @@
+const User = require("../models/user");
+
+const signup = (req, res, next) => {
+    User.find({ email: req.body.email }) // provera da li postoji user sa tim mejlom
+        .exec()
+        .then((user) => {
+            if (user.length >= 1) {
+                // jer ako ne postoji ne baca null nego prazan string
+                return res.status(422).json({
+                    message: "Mail exists", // ako postoji baca se greska
+                });
+            } else {
+                // ako ne postoji nastavljas dalje i provera passworda
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: err,
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash,
+                        });
+                        user.save()
+                            .then((result) => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created",
+                                });
+                            })
+                            .catch((err) => {
+                                res.status(500).json({
+                                    error: err,
+                                });
+                            });
+                    }
+                }); // salting rounds je 10
+            }
+        });
+}
+
+const login = (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then((user) => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed!",
+                });
+            }
+            bcrypt.compare(
+                req.body.password,
+                user[0].password,
+                (err, result) => {
+                    if (err) {
+                        return res.status(401).json({
+                            message: "Auth failed!",
+                        });
+                    }
+                    if (result) {
+                        const token = jwt.sign(
+                            {
+                                email: user[0].email,
+                                userId: user[0]._id,
+                            },
+                            "secret",
+                            {
+                                expiresIn: "1h",
+                            }
+                        );
+                        return res.status(200).json({
+                            message: "Auth successful",
+                            token: token,
+                        });
+                    }
+                    res.status(401).json({
+                        message: "Auth failed!",
+                    });
+                }
+            );
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: err,
+            });
+        });
+}
+
+const deleteUser = (req, res, next) => {
+    User.deleteOne({ _id: req.params.userId })
+        .exec()
+        .then((result) => {
+            res.status(200).json({
+                message: "User deleted",
+            });
+        })
+        .catch((err) =>
+            res.status(500).json({
+                error: err,
+            })
+        );
+}
+
+module.exports = {
+    signup,
+    login,
+    deleteUser
+}
